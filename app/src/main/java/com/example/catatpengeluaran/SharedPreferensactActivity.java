b9011bb6d87397f6a92498a1db792a8eae8f7421
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-07-08 11:43:12 +0700

    materi pertemuan tanggal 8 juli 2023

--- app/src/main/java/com/example/catatpengeluaran/SharedPreferensactActivity.java
+++ app/src/main/java/com/example/catatpengeluaran/SharedPreferensactActivity.java
@@ -2,6 +2,7 @@ package com.example.catatpengeluaran;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Button;
@@ -23,5 +24,24 @@ public class SharedPreferensactActivity extends AppCompatActivity {
         setContentView(R.layout.activity_shared_preferensact);
 
         sharePrevManager = new SharePrevManager(this);
+
+        if (sharePrevManager.getIsLogin()){
+            //Kondisi Jika User Sudah Login
+            startHomeUI();
+        } else {
+            //Kondisi Jika User Belum Log in
+            etUsername = findViewById(R.id.et_nama);
+            etPassword = findViewById(R.id.et_password);
+            btnlogin = findViewById(R.id.btnLogin);
+            pbLogin = findViewById(R.id.pblogin);
+            ivLogin = findViewById(R.id.login);
+        }
+    }
+
+    public void startHomeUI(){
+        Intent i = new Intent(SharedPreferensactActivity.this,LoginActivity.class);
+
+        startActivity(i);
+        finishAffinity();
     }
 }
